26378f03f9e3bef86a41fcc2ef77a16bdfc228d7
Barathraj-M <barathrajbrix25@gmail.com>
2025-11-04 06:18:32 +0530

    Setup

---
+++ backend/tests/task.test.js
@@ -0,0 +1,21 @@
+const request= require("supertest")
+const {app,server} = require("../index")
+const mongoose = require("mongoose")
+
+describe('Get API Tasks',()=>{
+    it('it should return 200 ok',async()=>{
+        const res = await request(app).get('/api/tasks')
+        expect(res.statusCode).toEqual(200)
+    })
+    it('it should return Object data',async()=>{
+        const res = await request(app).get('/api/tasks')
+        // expect(Array.isArray(res.body)).toBe(true)
+        expect(typeof res.body).toBe("object")
+        expect(res.body).toHaveProperty('tasks')
+    })
+})
+
+afterAll(async()=>{
+    await mongoose.connection.close()
+    server.close()
+})
